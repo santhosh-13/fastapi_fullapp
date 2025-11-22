974b7c47bf0d19d6eaabc5eeff72f028e64de0aa
unknown <santhoshkoppisetti2002@gmail.com>
2025-11-22 19:56:04 +0530

    updated

--- app/config.py
+++ app/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     secret_key:str
     algorithm:str
     access_token_expire_minutes:int
+    database_path:str
 
     class Config:
         env_file=".env"
--- app/database.py
+++ app/database.py
@@ -7,7 +7,8 @@ import time
 from .config import settings
 
 
-SQLALCHEMY_DATABASE_URL=f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+#SQLALCHEMY_DATABASE_URL=f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL=settings.database_path
 engine=create_engine(SQLALCHEMY_DATABASE_URL) #establish connection
 
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine) #talk to database
