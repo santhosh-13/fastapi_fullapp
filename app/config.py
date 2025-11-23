8217a99106976bf964f72fbe62a7ca9bd17d80fb
unknown <santhoshkoppisetti2002@gmail.com>
2025-11-23 14:16:37 +0530

    updated

--- app/config.py
+++ app/config.py
@@ -1,18 +1,13 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    database_hostname:str
-    database_password:str
-    database_port:str
-    database_name:str
-    database_username:str
-    secret_key:str
-    algorithm:str
-    access_token_expire_minutes:int
-    database_path:str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+    database_path: str
 
     class Config:
         env_file=".env"
 
-
-settings=Settings()
+settings = Settings()
+  
